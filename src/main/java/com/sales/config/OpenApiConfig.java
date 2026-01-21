48efc3ee8c5a6a432f86cbb9e91ff79394c9ba71
nareshswami199 <swaminaresh993@gmail.com>
2026-01-21 23:25:58 +0530

    improved jwt filter and configure with swagger

---
+++ src/main/java/com/sales/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.sales.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Bearer Token. Enter token **without** 'Bearer ' prefix",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sales API")
+                        .version("1.0.0")
+                        .description("API Documentation with JWT Bearer Authentication"));
+    }
+}
--- src/main/java/com/sales/config/SwaggerConfiguration.java
+++ src/main/java/com/sales/config/SwaggerConfiguration.java
@@ -5,14 +5,14 @@ import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import org.springdoc.core.customizers.OperationCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
 
 @Configuration
 public class SwaggerConfiguration {
 
-    @Bean
+    // TODO : currently we are not using this. because this is used for headers
+    //@Bean
     public OperationCustomizer customGlobalHeaders() {
 
         return (Operation operation, HandlerMethod handlerMethod) -> {
